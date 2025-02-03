262031aea2a84ea4d65f7585049ba9f6d8c6c71f
Wayne Hoggett <wayne-hoggett@pluralsight.com>
2025-02-04 07:58:59 +1000

    Updates

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3d17b7ed86b0bb6b5d4a490ff2adde79858f2fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+675be8d2c2b5ea7c48c5af78e722bcf2264670e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
